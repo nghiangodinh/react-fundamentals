b3744e2242312b63a12d4d964ec939f6204637b7
nghiango <nghiangodinh@yahoo.com>
2016-11-21 17:57:23 +0700

    add initial files

---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+/*eslint-disable strict */ //Disabling check because we can't run strict mode. Need global vars.
+
+var React = require('react');
+var RouteHandler = require('react-router').RouteHandler;
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <div className="container-fluid">
+                    <RouteHandler/>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = App;
